8126b699303762e5fdb23eb269e9e1c325b7fd45
Lena <57621352+Lenam0n@users.noreply.github.com>
2024-08-21 09:05:06 +0200

    Update PracGegner.py

--- PracGegner.py
+++ PracGegner.py
@@ -5,6 +5,7 @@ import os
 import json
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from notion_client import Client
