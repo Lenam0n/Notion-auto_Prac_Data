ba3023d06b17ff2b3eeb3d9bfa031c11ec89ae4d
Lena <57621352+Lenam0n@users.noreply.github.com>
2024-08-22 09:55:03 +0200

    add map icons

--- PracGegner.py
+++ PracGegner.py
@@ -10,9 +10,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from notion_client import Client
 import re
-from dotenv import load_dotenv
 
-load_dotenv()
 GOOGLE_KALENDER_ID = os.getenv("GOOGLE_KALENDER_ID")
 NOTION_PRAC_LIST = os.getenv("NOTION_PRAC_LIST")
 NOTION_ENEMY_LIST = os.getenv("NOTION_ENEMY_LIST")
@@ -109,7 +107,13 @@ def create_page_in_prac_list(date, map_name, gegner_id):
                     {"id": gegner_id}
                 ]
             }
-        }
+        },
+        "icon":{
+                "type": "external",
+                "external": {
+                    "url": mapSelect(map_name)
+                }
+}
     }
     notion.pages.create(**new_page)
 
@@ -174,6 +178,33 @@ def google_auth():
     creds = Credentials.from_service_account_info(token_info,scopes=SCOPES)
     return creds
 
+def mapSelect(map_name):
+    if map_name == "Ascent":
+        return "https://media.valorant-api.com/maps/7eaecc1b-4337-bbf6-6ab9-04b8f06b3319/splash.png"
+    elif map_name == "Split":
+        return "https://media.valorant-api.com/maps/d960549e-485c-e861-8d71-aa9d1aed12a2/splash.png"
+    elif map_name == "Fracture":
+        return "https://media.valorant-api.com/maps/b529448b-4d60-346e-e89e-00a4c527a405/splash.png"
+    elif map_name == "Bind":
+        return "https://media.valorant-api.com/maps/2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba/splash.png"
+    elif map_name == "Breeze":
+        return "https://media.valorant-api.com/maps/2fb9a4fd-47b8-4e7d-a969-74b4046ebd53/splash.png"
+    elif map_name == "Abyss":
+        return "https://media.valorant-api.com/maps/224b0a95-48b9-f703-1bd8-67aca101a61f/splash.png"
+    elif map_name == "Lotus":
+        return "https://media.valorant-api.com/maps/2fe4ed3a-450a-948b-6d6b-e89a78e680a9/splash.png"
+    elif map_name == "Sunset":
+        return "https://media.valorant-api.com/maps/92584fbe-486a-b1b2-9faa-39b0f486b498/splash.png"
+    elif map_name == "Pearl":
+        return "https://media.valorant-api.com/maps/fd267378-4d1d-484f-ff52-77821ed10dc2/splash.png"
+    elif map_name == "Icebox":
+        return "https://media.valorant-api.com/maps/e2ad5c54-4114-a870-9641-8ea21279579a/splash.png"
+    elif map_name == "Haven":
+        return "https://media.valorant-api.com/maps/2bee0dc9-4ffe-519b-1cbd-7fbe763a6047/splash.png"
+    #elif map_name == "Pearl":
+        #return ""
+    else:
+        return ""
 
 
 def main():
