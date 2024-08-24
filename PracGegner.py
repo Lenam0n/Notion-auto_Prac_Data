60dd0d98773e49fa117433e56edb0fc3aed232a4
Lena <57621352+Lenam0n@users.noreply.github.com>
2024-08-24 22:29:03 +0200

    Update PracGegner.py

--- PracGegner.py
+++ PracGegner.py
@@ -272,7 +272,7 @@ def main():
 
     # Startzeitpunkt auf 7 Tage zurücksetzen (oder nach Wunsch anpassen)
     time_min = (now - timedelta(days=21)).isoformat() + 'Z'
-    time_max = (now.isoformat() + timedelta(hours=2)) + 'Z'
+    time_max = (now + timedelta(hours=2)).isoformat() + 'Z'
     
     # Abrufen der letzten 10 Ereignisse aus der Vergangenheit
     events_result = service.events().list(calendarId=GOOGLE_KALENDER_ID, timeMin=time_min,
